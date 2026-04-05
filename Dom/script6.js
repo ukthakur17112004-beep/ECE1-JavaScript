b39be31030410665a1a9d5bf96b30ef36924b8c8
Utkarsh Thakur <utkarsh.25b03110199@abes.ac.in>
2026-04-06 01:31:14 +0530

    Experiment 1

---
+++ Dom/script6.js
@@ -0,0 +1,20 @@
+function onClick(){
+    console.log("on mouse over");
+
+}
+
+function onMouseDown(){
+    console.log("on mouse down ");
+}
+
+function onMouseup(){
+    console.log("on mouse up ");
+}
+
+function onMouseover(){
+    console.log("on mouse over ");
+}
+
+function onMouseDown(){
+    console.log("on mouse down ");
+}
