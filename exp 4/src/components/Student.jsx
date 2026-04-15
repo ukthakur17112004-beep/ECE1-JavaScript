52aab8b3b7e31752d914c0fd99935b3a56662a61
Utkarsh Thakur <utkarsh.25b03110199@abes.ac.in>
2026-04-15 20:13:32 +0530

    Exp 4

---
+++ exp 4/src/components/Student.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Student() {
+  return (
+    <div>
+      <div className="student-card">
+        <h2>{name}</h2>
+        <p>{course}</p>
+        <p>{marks}</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Student
