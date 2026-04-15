727dd74864765d374d8b45e85854e08ab59df20f
Utkarsh Thakur <utkarsh.25b03110199@abes.ac.in>
2026-04-15 20:09:53 +0530

    App

---
+++ ece 1/src/App.jsx
@@ -0,0 +1,13 @@
+import Student from './components/Student'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Student/>
+    </>
+  )
+}
+
+export default App
---
+++ ece 1/src/components/Student.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Student(name, age) {
+  return (
+    <div>
+        <h1>Name: {Utkarsh}</h1>   
+        <h1>Age: {19}</h1>
+    </div>
+  )
+}
+
+export default Student
